a6264ece0898bf39a1fbc14cdeb1c5cd10785434
anthony-roux <roux.anthony@hetic.net>
2020-03-16 08:56:26 +0100

    add queryselector and addEventListener for burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,13 @@ btn.addEventListener('click', () => {
     clicked = true;
   }
 });
+
+var burger = document.querySelector('.nav__menu-button');
+var menu = document.querySelector('.menu');
+var header = document.querySelector('.header');
+
+burger.addEventListener('click', () => {
+  menu.classList.toggle('.is-open-menu')
+  header.classList.toggle('.header-is-open')
+
+});
