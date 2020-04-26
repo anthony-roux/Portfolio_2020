3aef4bb6f37c2fb4ddb7a6f978f696a52fef6e0f
anthony-roux <roux.anthony@hetic.net>
2020-04-26 19:52:37 +0200

    create animation with GSAP and ScrollLMagic

--- src/js/scripts/gsap_scrollMagic.js
+++ src/js/scripts/gsap_scrollMagic.js
@@ -2,7 +2,7 @@
 
 const controller = new ScrollMagic.Controller();
 
-//tween
+//tweens
 const tween_vectorMe = TweenMax.from('#vectorMe', 1, {
   scale: 0.9,
   duration: '80%',
@@ -25,12 +25,123 @@ const tween_text_hero = TweenMax.staggerFromTo(
   0.1
 );
 
+const tween_themeOtaku = TweenMax.staggerFromTo(
+  '.tween_themeOtaku',
+  0.75,
+  {
+    opacity: 0,
+    x: -300,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    x: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+
+const tween_themeSocoda = TweenMax.staggerFromTo(
+  '.tween_themeSocoda',
+  0.75,
+  {
+    opacity: 0,
+    x: -300,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    x: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+
+const tween_themeNova = TweenMax.staggerFromTo(
+  '.tween_themeNova',
+  0.75,
+  {
+    opacity: 0,
+    x: -300,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    x: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+
+const tween_themeKoala = TweenMax.staggerFromTo(
+  '.tween_themeKoala',
+  0.75,
+  {
+    opacity: 0,
+    x: -300,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    x: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+const tween_themeNespresso = TweenMax.staggerFromTo(
+  '.tween_themeNespresso',
+  0.75,
+  {
+    opacity: 0,
+    x: -300,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    x: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+
+const tween_themeIkea = TweenMax.staggerFromTo(
+  '.tween_themeIkea',
+  0.75,
+  {
+    opacity: 0,
+    x: -300,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    x: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+
 const tween_text_aboutMe = TweenMax.staggerFromTo(
   '.tween_text_aboutMe',
   0.75,
   {
     opacity: 0,
     y: -100,
+    ease: Power4.easeInOut
+  },
+  {
+    opacity: 1,
+    y: 0,
+    ease: Power4.easeInOut
+  },
+  0.1
+);
+
+const tween_text_skills = TweenMax.staggerFromTo(
+  '.tween_text_skills',
+  0.75,
+  {
+    opacity: 0,
+    y: -30,
     ease: Power4.easeInOut
   },
   {
@@ -57,6 +168,90 @@ const scene_vectorMe = new ScrollMagic.Scene({
   .reverse(false)
   .addTo(controller);
 
+const scene_themeOtaku = new ScrollMagic.Scene({
+  triggerElement: '#animate26',
+  offset: 500
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'Theme_otaku',
+  //   colorTrigger: 'red',
+  //   indent: 300
+  // })
+  .setTween(tween_themeOtaku)
+  .reverse(false)
+  .addTo(controller);
+
+const scene_themeSocoda = new ScrollMagic.Scene({
+  triggerElement: '#animate25',
+  offset: -200
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'Theme_socoda',
+  //   colorTrigger: 'red',
+  //   indent: 300
+  // })
+  .setTween(tween_themeSocoda)
+  .reverse(false)
+  .addTo(controller);
+
+const scene_themeNova = new ScrollMagic.Scene({
+  triggerElement: '#animate24',
+  offset: -200
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'Theme_Nova',
+  //   colorTrigger: 'red',
+  //   indent: 300
+  // })
+  .setTween(tween_themeNova)
+  .reverse(false)
+  .addTo(controller);
+
+const scene_themeKoala = new ScrollMagic.Scene({
+  triggerElement: '#animate23',
+  offset: -200
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'Theme_Koala',
+  //   colorTrigger: 'red',
+  //   indent: 300
+  // })
+  .setTween(tween_themeKoala)
+  .reverse(false)
+  .addTo(controller);
+
+const scene_themeNespresso = new ScrollMagic.Scene({
+  triggerElement: '#animate22',
+  offset: -200
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'Theme_Nespresso',
+  //   colorTrigger: 'red',
+  //   indent: 300
+  // })
+  .setTween(tween_themeNespresso)
+  .reverse(false)
+  .addTo(controller);
+
+const scene_themeIkea = new ScrollMagic.Scene({
+  triggerElement: '#animate21',
+  offset: -300
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'Theme_Ikea',
+  //   colorTrigger: 'red',
+  //   indent: 300
+  // })
+  .setTween(tween_themeIkea)
+  .reverse(false)
+  .addTo(controller);
+
 const scene_aboutMe = new ScrollMagic.Scene({
   triggerElement: '#animate3',
   offset: -100
@@ -68,5 +263,19 @@ const scene_aboutMe = new ScrollMagic.Scene({
   //   indent: 300
   // })
   .setTween(tween_text_aboutMe)
+  .reverse(false)
+  .addTo(controller);
+
+const scene_skills = new ScrollMagic.Scene({
+  triggerElement: '#animate4',
+  offset: -250
+})
+  .triggerHook(0.4)
+  // .addIndicators({
+  //   name: 'AboutMe_txt',
+  //   colorTrigger: 'red',
+  //   indent: 50
+  // })
+  .setTween(tween_text_skills)
   .reverse(false)
   .addTo(controller);
